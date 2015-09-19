e9eea528cf90644e9cb621a251c63dd381a8251d
Daniel Blignaut <danielblignaut@gmail.com>
2015-09-19 16:09:15 +0200

    edited line length to 79 characters. Removed all semi colons

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -6,27 +6,32 @@ import entertainment_center;
 import os;
 
 
-'''the PATH global variabbles are used to help locate the data source file & the client angular app'''
-DATABASE = os.path.dirname(os.path.realpath(__file__)) + "/datasource.json";
-CLIENT = os.path.dirname(os.path.realpath(__file__)) + "/static/";
+'''the PATH global variabbles are used to help locate the data source file & 
+    the client angular app'''
+DATABASE = os.path.dirname(os.path.realpath(__file__)) + "/datasource.json"
+CLIENT = os.path.dirname(os.path.realpath(__file__)) + "/static/"
 
-''' Initiate Flask & fetch the list of movies. Also ensure that the server allows cross origin domain requests so that
-our client can get JSON data from the server (it\'s hosted on a seperate server. '''
+''' Initiate Flask & fetch the list of movies. Also ensure that the server 
+    allows cross origin domain requests so that
+    our client can get JSON data from the server (it\'s hosted on a seperate
+    server. '''
 
-app = Flask(__name__, static_url_path=CLIENT);
-Center = entertainment_center.Entertainment_Center(DATABASE);
+app = Flask(__name__, static_url_path=CLIENT)
+Center = entertainment_center.Entertainment_Center(DATABASE)
 
-'''route a GET request through the API to return the number of movies in the list '''
+'''route a GET request through the API to return the number of movies in the 
+    list '''
 
 @app.route('/api/v1/movies', methods=['GET'])
 def get_movies():
     return "{ \"movies\": " + Center.toString() + "}";
 
-'''route a GET request through the API to return the number of movies in the list '''
+'''route a GET request through the API to return the number of movies in the 
+    list '''
 
 @app.route('/api/v1/count', methods=['GET'])
 def get_count() :
-    return "{\"count\": \"" + str(Center.getCount()) +"\"}";
+    return "{\"count\": \"" + str(Center.getCount()) +"\"}"
 
 '''open the output file in the browser (in a new tab, if possible)
 the index.html file is responsible for querying the above end points and 
@@ -34,10 +39,10 @@ fetching the python data strucutres to populate the website'''
 
 @app.route('/', methods=['GET'])
 def index():
-    return send_from_directory(CLIENT, 'index.html');
+    return send_from_directory(CLIENT, 'index.html')
 
 if __name__ == '__main__':
-	app.run(debug=True, use_reloader=True, host='0.0.0.0', port=8000);
+	app.run(debug=True, use_reloader=True, host='0.0.0.0', port=8000)
     
 
     
--- media.py
+++ media.py
@@ -1,37 +1,37 @@
 class Movie() :
     """ This class provides a way to store movie related data """
     
-    VALID_RESTRICTIONS = ["G", "PG", "PG-13", "R"];
+    VALID_RESTRICTIONS = ["G", "PG", "PG-13", "R"]
     
     def __init__(self, title, storyline, image_poster, youtube_trailer, rating, restriction) :
-        self.title = title;
-        self.storyline = storyline;
-        self.image_poster = image_poster;
-        self.youtube_trailer = youtube_trailer;
+        self.title = title
+        self.storyline = storyline
+        self.image_poster = image_poster
+        self.youtube_trailer = youtube_trailer
 
         if(Movie.check_rating(rating)) :
-        	self.rating = rating;
+        	self.rating = rating
         else :
-        	self.rating = "N/A";
+        	self.rating = "N/A"
 
         if(Movie.check_restriction(restriction)) :
-        	self.restriction = restriction.upper();
+        	self.restriction = restriction.upper()
         else :
-        	self.restriction = "N/A";
+        	self.restriction = "N/A"
 
    	'''this static method provides a way to check if the movies's age restriction is valid '''
     @staticmethod
     def check_restriction(restriction) :
     	for static_restriction in Movie.VALID_RESTRICTIONS :
             if(static_restriction == restriction.upper()) :
-                return True;
+                return True
 
-    	return False;
+    	return False
 
     '''this static method provides a way to check if the movies's rating is valid '''
     @staticmethod
     def check_rating(rating) :
    		if(rating >= 0 and rating <= 5) :
-   			return True;
+   			return True
 
-   		return False;
+   		return False
